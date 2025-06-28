a0dd5cde149287be674a24abe41a467aefcd5bae
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-28 16:11:21 +0600

    explored the app file and more

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <a href="/about">about</a>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
