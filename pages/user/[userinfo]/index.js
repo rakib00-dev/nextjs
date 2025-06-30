802353a6f09f92c85b322bce9633c6c119c2db1c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-30 17:02:43 +0600

    added the button to change page

---
+++ pages/user/[userinfo]/index.js
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+
+export default () => {
+  const router = useRouter();
+  return (
+    <>
+      <h1>This is user {router.query.userinfo} index page</h1>;
+      <button
+        // onClick={() => router.push(`/user/${router.query.userinfo}/settings`)}
+        onClick={() =>
+          router.push({
+            pathname: 'user/[userinfo]/settings',
+            query: { userinfo: router.query.userinfo },
+          })
+        }
+      >
+        Open user settings
+      </button>
+    </>
+  );
+};
