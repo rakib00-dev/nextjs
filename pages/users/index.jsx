33a2089aa2bf399d7d62854b853054885c538c17
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-01 20:48:08 +0600

    learned moduler css in next js

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { fetcher } from './[id]';
 import useSWR from 'swr';
+import styles from '../../styles/User.module.css';
 
 const PagesUser = () => {
   const { data: user, error } = useSWR('https://dummyjson.com/users', fetcher);
@@ -10,7 +11,7 @@ const PagesUser = () => {
       <h1>Users</h1>
       {user?.users.map((e) => (
         <Link href={`/users/${e.id}`} key={e.id}>
-          <div>{e.firstName}</div>
+          <div className={styles['text-color']}> {e.firstName}</div>
         </Link>
       ))}
     </>
